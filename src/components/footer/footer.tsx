dfd8d33b1f68a83eea1c771ac3f55ec6a0ff6f6e
roosterruler <88428487+bhhoang818@users.noreply.github.com>
2024-09-23 00:04:25 +0700

    add code

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Container from '../common/Container'
+import { Link } from '@nextui-org/react'
 
 const Footer: React.FC = () => {
     return (
-        <footer>asd</footer>
+        <footer>
+            <Container className=''>
+                <div className="grid grid-cols-3">
+                    <div className="logo">
+                        <Link href={''} className='hover:opacity-100 w-[150px] sm:w-[200px]'>
+                            <img src="/assets/logo.png" alt="" />
+                        </Link>
+                    </div>
+                </div>
+            </Container>
+        </footer>
     )
 }
 
