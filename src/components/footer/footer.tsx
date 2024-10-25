b7865f89059eecd77cd92a2321fedf2a6d788f3d
roosterruler <88428487+bhhoang818@users.noreply.github.com>
2024-10-25 22:11:20 +0700

    add

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import Container from '../common/Container'
+// import { IconBrandFacebook, IconBrandGithub, IconBrandInstagram, IconBrandLinkedin } from '@tabler/icons-react';
 import { Link } from '@nextui-org/react'
+import Container from '../common/Container'
 import Typography from '../common/Typography'
 import { ListboxWrapper } from '../common/ListboxWrapper'
 import { ListItem } from '../common/ListItem'
@@ -10,29 +11,56 @@ import Col from '../common/Col'
 const Footer: React.FC = () => {
     const items = [
         {
-            key: "new",
-            label: "New file",
-            href: '/'
+            key: "about",
+            label: "Giới thiệu",
+            href: '/about'
         },
         {
-            key: "copy",
-            label: "Copy link",
-            href: '/'
+            key: "service",
+            label: "Dịch vụ",
+            href: '/service'
         },
         {
-            key: "edit",
-            label: "Edit file",
-            href: '/'
+            key: "project",
+            label: "Dự án",
+            href: '/project'
         },
         {
-            key: "delete",
-            label: "Delete file",
-            href: '/'
+            key: "contact",
+            label: "Liên hệ",
+            href: '/contact'
         }
     ];
+    // const social = [
+    //     {
+    //         key: "facebook",
+    //         label: "Facebook",
+    //         href: '/',
+    //         icon: `${<IconBrandFacebook />}`
+    //     },
+    //     {
+    //         key: "instagram",
+    //         label: "Instagram",
+    //         href: '/',
+    //         icon: `${<IconBrandInstagram />}`
+    //     },
+    //     {
+    //         key: "github",
+    //         label: "Github",
+    //         href: '/',
+    //         icon: `${<IconBrandGithub />}`
+    //     },
+    //     {
+    //         key: "linkedin",
+    //         label: "LinkedIn",
+    //         href: '/',
+    //         icon: `${<IconBrandLinkedin />}`
+    //     }
+    // ];
+
     return (
         <footer className='py-20'>
-            <Container className=''>
+            <Container className='max-w-5xl'>
                 <Row>
                     <Col full={true} md={4}>
                         <div className="logo">
@@ -55,7 +83,7 @@ const Footer: React.FC = () => {
                     </Col>
                     <Col full={true} md={4}>
                         <div className="item">
-                            <Typography as='h5' className='text-xl font-bold'>Dịch vụ của chúng tôi</Typography>
+                            <Typography as='h5' className='text-xl font-bold'>Kết nối với chúng tôi</Typography>
                             <ListboxWrapper>
                                 {
                                     items.map((item, index) => (
