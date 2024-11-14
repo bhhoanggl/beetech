c92234bd8375aada17d1ff8007ddb29ba54cab00
roosterruler <88428487+bhhoang818@users.noreply.github.com>
2024-11-14 22:12:00 +0700

    Merge branch 'main' of https://github.com/bhhoanggl/beetech
    # Conflicts:
    #	src/components/footer/footer.tsx
    #	vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig(({ mode }: ConfigEnv) => {
     css: {
       preprocessorOptions: {
         scss: {
-          api: 'modern-compiler' // or "modern"
+          api: 'modern-compiler'
         }
       }
     }
